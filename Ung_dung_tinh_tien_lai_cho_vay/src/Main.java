c41657e76b6ddfef9616ba9fe0c0730ed40b88a0
Cai Nhan <57951047+NhanCaii@users.noreply.github.com>
2023-10-06 11:10:42 +0700

    Add files via upload

---
+++ Ung_dung_tinh_tien_lai_cho_vay/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double money = 1.0;
+        int month;
+        double interestRate;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.print("Enter number of months: ");
+        month = input.nextInt();
+        System.out.print("Enter annual interest rate in percentage: ");
+        interestRate = input.nextDouble();
+
+        double totalInterest = 0;
+        for(int i = 0; i < month; i++){
+            totalInterest += money * (interestRate/100)/12 * month;
+        }
+        System.out.println("Total of interest: " + totalInterest);
+
+    }
+}
